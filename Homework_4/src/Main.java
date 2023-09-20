36c3af7a8c380e0d01d47d718e47e25d0b5b1f4b
Kio <kio.m@gmail.com>
2023-09-20 20:06:10 +0300

    Homework_4

--- Homework_4/src/Main.java
+++ Homework_4/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        int smallest = 1;
+        boolean found = false;
+        while (!found)
+        {
+            if (smallest % 387 == 0 && smallest % 6381 == 0)
+                found = true;
+            else
+                smallest = smallest + 1;
+        }
+        System.out.println(smallest);
+
     }
 }
