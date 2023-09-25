7d95b9bda9e3e7ea24c21295167e1afcdd72e624
Kio <kio.m@gmail.com>
2023-09-25 09:10:36 +0300

    Homework_3

--- Homework_3/src/Main.java
+++ Homework_3/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args)
     {
-
+        int A = 43, B = 14;
+        boolean last_digit = false;
+        if (A % 10 == B % 10)
+            last_digit = true;
+        System.out.println(last_digit);
     }
 }
